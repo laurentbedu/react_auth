ff1949065963bb8fc4309d1e3ac10adabb830e46
laurentbedu <bedulaurent@gmail.com>
2022-02-22 14:06:20 +0100

    in progress

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -47,20 +47,26 @@ function NavBar() {
             )}
           </ul>
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              {auth.role === 0 && (
+            {auth.role === 0 && (
+              <li className="nav-item">
                 <Link className="nav-link" to="/login">
                   Login
                 </Link>
-              )}
-            </li>
-            <li className="nav-item">
-              {auth.role === 0 && (
+              </li>
+            )}
+            {auth.role === 0 && (
+              <li className="nav-item">
                 <Link className="nav-link" to="/register">
                   Register
                 </Link>
-              )}
-            </li>
+              </li>
+            )}
+            {auth.role > 0 && (
+                <li className="nav-item">
+                    Logout
+                </li>
+                
+            )}
           </ul>
         </div>
       </div>
